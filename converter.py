57bbdf85e20dca4030f62dfe9e76bad8ef0a67dd
CapSOSkw <wky0702@gmail.com>
2018-06-14 14:50:13 -0400

    Serial: 129s, Multiprocessing: 36~39s; for range(9999999)

--- converter.py
+++ converter.py
@@ -1,4 +1,6 @@
 import re
+import time
+import multiprocessing as mp
 
 _chineseDigit = {
     'Lower': {
@@ -50,16 +52,16 @@ def converter(num: int, lower=True):
                     num %= 10 ** num_of_digit
 
         elif 4 < num_length <= 8:
-            ten_thousand = num // 10 ** 4
+            ten_thousand = num // 10000
             result += converter(ten_thousand, lower=lower) + '万'
-            num %= 10 ** 4
+            num %= 10000
             if num < 10:
                 result += '零'
 
-        elif 8 < num_length:
-            ten_billion = num // 10 ** 8
+        else:
+            ten_billion = num // 100000000
             result += converter(ten_billion, lower=lower) + '亿'
-            num %= 10 ** 8
+            num %= 100000000
             if num < 10:
                 result += '零'
 
@@ -71,4 +73,16 @@ def converter(num: int, lower=True):
 
 
 if __name__ == '__main__':
-    print(converter(101, lower=True))
+    # t1 = time.time()
+    #
+    # for i in range(9999999):
+    #     converter(i)
+    #
+    # print(time.time() - t1) # 129.48s
+
+    t2 = time.time()
+    with mp.Pool(processes=6) as pool:
+        pool.map(converter, range(9999999))
+
+    print(time.time() - t2)  # 36 ~ 39s
+
